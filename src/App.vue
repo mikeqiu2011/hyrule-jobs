8c6459597ceceb073f67aae12fbe6f4d2ebcfaff
mikeqiu2011 <mikeqiu2011@163.com>
2022-04-05 17:09:05 +0800

    interface to declare Job type

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,46 @@
 <template>
   <div class="app">
-    <p>{{ name }} - {{ age }}</p>
-    <button @click="changeName('mike')">change name</button>
-    <button @click="changeAge('19')">change age</button>
+    <p>{{ jobs[0].location }}</p>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, ref } from "vue";
+import { Job } from "./types/Job";
 
 export default defineComponent({
   name: "App",
   components: {},
   setup() {
-    const name = ref("Link");
-    const age = ref<number | string>(25);
+    const jobs = ref<Job[]>([
+      {
+        title: "farm worker",
+        location: "lon lon ranch",
+        id: "1",
+        salary: 30000,
+      },
+      {
+        title: "quarryman",
+        location: "death mountain",
+        salary: 40000,
+        id: "2",
+      },
+      {
+        title: "flute player",
+        location: "the lost woods",
+        salary: 35000,
+        id: "3",
+      },
+      { title: "fisherman", location: "lake hylia", salary: 21000, id: "4" },
+      {
+        title: "prison guard",
+        location: "gerudo valley",
+        salary: 32000,
+        id: "5",
+      },
+    ]);
 
-    age.value = 30;
-
-    return { name, age };
-  },
-  methods: {
-    changeName(name: string) {
-      this.name = name;
-    },
+    return { jobs };
   },
 });
 </script>
